cc441610da63bd793be8c605795fa32d880e4a56
windows <windows@DESKTOP-RI5E4D7>
2025-11-29 17:34:52 +0200

    Merge branch 'master' of https://github.com/brookyox/oop-UNO-game.git

--- oop-UNO-game/src/uno/Deck.java
+++ oop-UNO-game/src/uno/Deck.java
@@ -1,85 +1,45 @@
 package uno;
 
 import java.util.Stack;
+import java.util.Collections;
+
 public class Deck {
 	private Stack<Card> deck = new Stack<>();
 	public Deck() {
-		deck.push(new NumberCard(Color.BLUE, 0));
-		
-		for (int i = 1; i <= 9; i++) {
-		    deck.push(new NumberCard(Color.BLUE, i));
-		    deck.push(new NumberCard(Color.BLUE, i));
-		}
+	    Color[] unoColors = new Color[4];
+	    unoColors[0] = Color.BLUE;
+	    unoColors[1] = Color.RED;
+	    unoColors[2] = Color.YELLOW;
+	    unoColors[3] = Color.GREEN;
 
-		deck.push(new BlockCard(Color.BLUE));
-		deck.push(new BlockCard(Color.BLUE));
+	    for (Color currentColor : unoColors) {
+	        deck.push(new NumberCard(currentColor, 0));
+	        for (int number = 1; number <= 9; number++) {
+	            deck.push(new NumberCard(currentColor, number));
+	            deck.push(new NumberCard(currentColor, number));
+	        }
 
-		deck.push(new ReverseCard(Color.BLUE));
-		deck.push(new ReverseCard(Color.BLUE));
+	        deck.push(new BlockCard(currentColor));
+	        deck.push(new BlockCard(currentColor));
 
-		deck.push(new Plus2Card(Color.BLUE));
-		deck.push(new Plus2Card(Color.BLUE));
-		
-		deck.push(new NumberCard(Color.RED, 0));
-		
-		for (int i = 1; i <= 9; i++) {
-		    deck.push(new NumberCard(Color.RED, i));
-		    deck.push(new NumberCard(Color.RED, i));
-		}
+	        deck.push(new ReverseCard(currentColor));
+	        deck.push(new ReverseCard(currentColor));
 
-		deck.push(new BlockCard(Color.RED));
-		deck.push(new BlockCard(Color.RED));
+	        deck.push(new Plus2Card(currentColor));
+	        deck.push(new Plus2Card(currentColor));
+	    }
 
-		deck.push(new ReverseCard(Color.RED));
-		deck.push(new ReverseCard(Color.RED));
-
-		deck.push(new Plus2Card(Color.RED));
-		deck.push(new Plus2Card(Color.RED));
-		
-		deck.push(new NumberCard(Color.YELLOW, 0));
-		
-		for (int i = 1; i <= 9; i++) {
-		    deck.push(new NumberCard(Color.YELLOW, i));
-		    deck.push(new NumberCard(Color.YELLOW, i));
-		}
-
-		deck.push(new BlockCard(Color.YELLOW));
-		deck.push(new BlockCard(Color.YELLOW));
-
-		deck.push(new ReverseCard(Color.YELLOW));
-		deck.push(new ReverseCard(Color.YELLOW));
-
-		deck.push(new Plus2Card(Color.YELLOW));
-		deck.push(new Plus2Card(Color.YELLOW));
-		
-		deck.push(new NumberCard(Color.YELLOW, 0));
-		
-		deck.push(new NumberCard(Color.GREEN, 0));
-		
-		for (int i = 1; i <= 9; i++) {
-		    deck.push(new NumberCard(Color.GREEN, i));
-		    deck.push(new NumberCard(Color.GREEN, i));
-		}
-
-		deck.push(new BlockCard(Color.GREEN));
-		deck.push(new BlockCard(Color.GREEN));
-
-		deck.push(new ReverseCard(Color.GREEN));
-		deck.push(new ReverseCard(Color.GREEN));
-
-		deck.push(new Plus2Card(Color.GREEN));
-		deck.push(new Plus2Card(Color.GREEN));
-		
-		for (int i = 0; i < 4; i++) {
-			deck.push(new WildCard());
-		    deck.push(new Wild4Card());
-		}
+	    for (int i = 0; i < 4; i++) {
+	        deck.push(new WildCard());
+	        deck.push(new Wild4Card());
+	    }
 	}
+
     
 	public Card draw() {
 		return deck.pop();
 	}
 	public void shuffle() {
-		
+		Collections.shuffle(deck);
 	}
 }
